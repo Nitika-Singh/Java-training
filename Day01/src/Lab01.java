5750338fea070afb5d398895d9da1ca51c5cb349
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-08-09 12:41:44 +0530

    Day01

---
+++ Day01/src/Lab01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        int target = (int)(Math.random() * 100);
+        int attempts = 0;
+        String message = "";
+        boolean isGameOver = false;
+        int guess = -1; //Some initial value
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the guessing game. Enter a number between 1 and 100");
+
+        while(!isGameOver) {
+            guess = scanner.nextInt();
+            attempts++;
+            if (guess > target) {
+                message = "Aim lower";
+            } else if(guess < target) {
+                message = "Aim Higher";
+            } else {
+                message = "You've got it in " + attempts + " attempts";
+                isGameOver = true;
+            }
+            System.out.println(message);
+        }
+    }
+}
