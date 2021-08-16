02f3167f80a6cc6a96d23b4f243490dfb55b837c
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-08-16 10:28:30 +0530

    Day04

---
+++ Day04/src/com/jpmc/lab05/Lab05.java
@@ -0,0 +1,16 @@
+package com.jpmc.lab05;
+
+public class Lab05 {
+    public static void main(String[] args) {
+        String input = "+5 -1 +9 +5 -7 +5 -3 +2 -4 +6 +8";
+        String[] inputArr = input.split(" ");
+        int sum = 0;
+        for (int i = 0; i < inputArr.length; i++) {
+            String numInStr = inputArr[i];
+            int num = Integer.parseInt(numInStr);
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+
+    }
+}
