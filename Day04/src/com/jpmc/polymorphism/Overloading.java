2c87b4c60701308e14a40b7f21eebffee5c90504
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-08-16 12:21:27 +0530

    Day04

---
+++ Day04/src/com/jpmc/polymorphism/Overloading.java
@@ -0,0 +1,12 @@
+package com.jpmc.polymorphism;
+
+public class Overloading {
+
+    public void printDetails() {}
+
+    public void printDetails(String data) {}
+
+    public void printDetails(String data1, String data2) {}
+
+    public void printDetails(long data) {}
+}
